8f95b04b3aefecfde8a854228b9181ac47373796
rpjackson2 <64040492+rpjackson2@users.noreply.github.com>
2021-01-22 08:07:47 -0600

    Can now change background to unlimited colors

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // set my color array
-const colors = [
-	"black",
-	"purple",
-	"green",
-	"yellow",
-	"indigo",
-	"blue",
-	"steelgray",
-	"orange",
-	"brown",
-	"violet",
-	"red",
-];
+// const colors = [
+// 	"black",
+// 	"purple",
+// 	"green",
+// 	"yellow",
+// 	"indigo",
+// 	"blue",
+// 	"steelgray",
+// 	"orange",
+// 	"brown",
+// 	"violet",
+// 	"red",
+// ];
 
-// 1hex codes have 6 digits 
+// 1hex codes have 6 digits
 // 2create a function that randomly chooses up to 6 numbers and letters
 
-// hec letter and numbers
-let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, a, b, c, d, e, f];
+// potential hex letters and numbers
+let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
 
-for (let num=0; num < hex.length;num++)
+//
 // select container element
 let contain = document.querySelector(".container");
 
@@ -29,18 +29,46 @@ let btn = document.querySelector("button");
 // select h1element
 let headline = document.querySelector("h1");
 
+// select h2element
+let hexh2 = document.querySelector("h2");
+
 // eventListener
 btn.addEventListener("click", function () {
 	// randomizer
 
-	let num = Math.floor(Math.random() * colors.length);
-
-	// setting background color
-	contain.style.backgroundColor = colors[num];
-	// setting h1 color
-	if (colors[num] === "black" || colors[num] === "purple") {
-		headline.style.color = "white";
-	} else {
-		headline.style.color = "black";
+	// empty string for newly created hex code
+	let hexString = "";
+	// for loop that iterates through hex array 6 times
+	for (let num = 0; num < 6; num++) {
+		// choose randomarray index
+		hexString += hex[Math.floor(Math.random() * hex.length)];
+		// concat new hex code
+		hexCode = `#${hexString}`;
+		// append hec code to h2
+		hexh2.innerHTML = hexCode;
+		// setting background color
+		contain.style.backgroundColor = hexCode;
 	}
+
+	// setting h1 color
+	// if (colors[num] === "black" || colors[num] === "purple") {
+	// 	headline.style.color = "white";
+	// } else {
+	// 	headline.style.color = "black";
+	// }
 });
+
+// btn.addEventListener("click", function () {
+// 	// randomizer
+
+// 	let num = Math.floor(Math.random() * colors.length);
+
+// 	// setting background color
+// 	contain.style.backgroundColor = colors[num];
+// 	// setting h1 color
+// 	if (colors[num] === "black" || colors[num] === "purple") {
+// 		headline.style.color = "white";
+// 	} else {
+// 		headline.style.color = "black";
+// 	}
+// });
